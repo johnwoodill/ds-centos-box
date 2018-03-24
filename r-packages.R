12189eda3cace36e7ff25cd19ad9a060572290db
johnwoodill <johnwoodill@gmail.com>
2018-03-24 09:56:26 -1000

    updt

--- r-packages.R
+++ r-packages.R
@@ -3,22 +3,22 @@ local({
   r["CRAN"] <- "http://cran.cnr.berkeley.edu/"
   options(repos = r)
 })
-
-install.packages(c("tidyverse",
-                   "stringr",
-                   "zoo",
-                   "lfe",
-                   "ggthemes",
-                   "RcppRoll",
-                   "rms",
-                   "noncensus",
-                   "maps",
-                   "foreign",
-                   "cowplot",
-                   "systemfit",
-                   "doParallel",
-                   "boot", 
-                   "sandwich",
-                   "lmtest",
-                   "devtools"))
+# 
+# install.packages(c("tidyverse",
+#                    "stringr",
+#                    "zoo",
+#                    "lfe",
+#                    "ggthemes",
+#                    "RcppRoll",
+#                    "rms",
+#                    "noncensus",
+#                    "maps",
+#                    "foreign",
+#                    "cowplot",
+#                    "systemfit",
+#                    "doParallel",
+#                    "boot", 
+#                    "sandwich",
+#                    "lmtest",
+#                    "devtools"))
 
