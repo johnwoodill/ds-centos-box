c3a41617407502300feca54f25c2e8f6eb2f1d19
johnwoodill <johnwoodill@gmail.com>
2018-03-23 18:51:52 -1000

    init commit

--- README.md
+++ README.md
@@ -5,9 +5,17 @@ Installation
 curl https://raw.githubusercontent.com/johnwoodill/ds-centos-box/master/ami-installer.sh > ami-installer.sh
 curl https://raw.githubusercontent.com/johnwoodill/ds-centos-box/master/r-packages.R > r-packages.R
 
-sudo passwd johnwoodill
-
+sudo usermod -aG wheel johnwoodill
 chmod +x ami-installer.sh
-sudo -s
-./ami-installer.sh
+sudo ./ami-installer.sh
+```
+
+Instructions
+------------
+
+``` bash
+
+# Uploading to Dropbox
+./dropbox_uploader.sh upload file.txt /Apps/file.txt
+
 ```
--- ami-installer.sh
+++ ami-installer.sh
@@ -2,25 +2,26 @@
 
 echo "johnwoodill:amstelthedog" | chpasswd
 
-usermod -aG wheel johnwoodill
+#usermod -aG wheel johnwoodill
 
 # Install necessary packages
-echo "Updating system ..."
+
+echo "$(tput setaf 2)-= UPDATING SYSTEM =-$(tput sgr0)"
 sleep 5
 yum -y update 
 
-echo "Installing packages ..."
+echo "$(tput setaf 2)-= INSTALLING PACKAGES =-$(tput sgr0)"
 sleep 5
 yum -y install epel-release wget htop libcurl libcurl-devel libxml2 libxml2-devel curl openssl openssl-devel postgresql postgresql-devel libpng libpng-devel libjpeg libjpeg-devel proj proj-devel proj-epsg proj-nad udunits2 udunits2-devel gdal gdal-devel gdal-libs R-java R-cpp libcurl-devel git
 
 yum -y groupinstall "Development Tools"
 
-echo "Installing R ..."
+echo "$(tput setaf 2)-= INSTALLING R =-$(tput sgr0)"
 sleep 5
 yum -y install R-core R-devel
 
 # Install rstudio server
-echo "Installing Rstudio Server ..."
+echo "$(tput setaf 2)-= INSTALLING RSTUDIO SERVER =-$(tput sgr0)"
 sleep 5
 wget https://download2.rstudio.org/rstudio-server-rhel-1.1.442-x86_64.rpm
 yum -y install rstudio-server-rhel-1.1.442-x86_64.rpm
@@ -28,19 +29,22 @@ sudo systemctl status rstudio-server.service
 sudo systemctl enable rstudio-server.service
 echo "www-port=80" >>  /etc/rstudio/rserver.conf
 
-echo "Installing R Packages ..."
+echo "$(tput setaf 2)-= INSTALLING R PACKAGES =-$(tput sgr0)"
 sleep 5
 Rscript r-packages.R
 
 su johnwoodill
 
 # Install Dropbox-Uploader (https://github.com/andreafabrizi/Dropbox-Uploader)
-echo "Installing Dropbox-uploader (https://github.com/andreafabrizi/Dropbox-Uploader) ..."
+echo "$(tput setaf 2)-= INSTALLING DROPBOX-UPLOADER =-$(tput sgr0)"
 sleep 5
 curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
 chmod +x dropbox_uploader.sh
-./dropbox_uploader.sh
+echo "K8Sh-q51ZRwAAAAAAAAVGrTPnEjSnj_VGvAYxfotUvfnfQba2pU7phDzhXJNvzMH\ny\n" | ./dropbox_uploader.sh
 
+echo "$(tput setaf 2)-= INSTALLATION COMPLETE. PRESS ENTER TO RESTART SERVER =-$(tput sgr0)"
+read 
+shutdown -r now
 
 
 # Install Anaconda
