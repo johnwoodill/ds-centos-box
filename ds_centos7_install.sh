deed71d7ff013f8f069e3d6a6b9b59c199ecaf33
johnwoodill <johnwoodill@gmail.com>
2018-03-24 17:27:11 -1000

    updt

--- ds_centos7_install.sh
+++ ds_centos7_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-echo "johnwoodill:amstelthedog" | chpasswd
+sudo echo "johnwoodill:amstelthedog" | chpasswd
 
-mkdir /home/johnwoodill/data
-mkdir /home/johnwoodill/models
+sudo mkdir /home/johnwoodill/data
+sudo mkdir /home/johnwoodill/models
 
 #usermod -aG wheel johnwoodill
 
@@ -11,26 +11,26 @@ mkdir /home/johnwoodill/models
 
 echo "$(tput setaf 2)-= UPDATING SYSTEM =-$(tput sgr0)"
 sleep 5
-yum -y update 
+sudo yum -y update 
 
 echo "$(tput setaf 2)-= INSTALLING PACKAGES =-$(tput sgr0)"
 sleep 5
-yum -y install epel-release wget htop libcurl libcurl-devel libxml2 libxml2-devel curl openssl openssl-devel postgresql postgresql-devel libpng libpng-devel libjpeg libjpeg-devel proj proj-devel proj-epsg proj-nad udunits2 udunits2-devel gdal gdal-devel gdal-libs R-java R-cpp libcurl-devel git
+sudo yum -y install epel-release wget htop libcurl libcurl-devel libxml2 libxml2-devel curl openssl openssl-devel postgresql postgresql-devel libpng libpng-devel libjpeg libjpeg-devel proj proj-devel proj-epsg proj-nad udunits2 udunits2-devel gdal gdal-devel gdal-libs R-java R-cpp libcurl-devel git
 
-yum -y groupinstall "Development Tools"
+sudo yum -y groupinstall "Development Tools"
 
 echo "$(tput setaf 2)-= INSTALLING R =-$(tput sgr0)"
 sleep 5
-yum -y install R-core R-devel
+sudo yum -y install R-core R-devel
 
 # Install rstudio server
 echo "$(tput setaf 2)-= INSTALLING RSTUDIO SERVER =-$(tput sgr0)"
 sleep 5
 wget https://download2.rstudio.org/rstudio-server-rhel-1.1.442-x86_64.rpm
-yum -y install rstudio-server-rhel-1.1.442-x86_64.rpm
+sudo yum -y install rstudio-server-rhel-1.1.442-x86_64.rpm
 sudo systemctl status rstudio-server.service
 sudo systemctl enable rstudio-server.service
-echo "www-port=80" >>  /etc/rstudio/rserver.conf
+sudo echo "www-port=80" >>  /etc/rstudio/rserver.conf
 
 echo "$(tput setaf 2)-= INSTALLING R PACKAGES =-$(tput sgr0)"
 sleep 5
@@ -40,13 +40,13 @@ Rscript r-packages.R
 echo "$(tput setaf 2)-= INSTALLING DROPBOX-UPLOADER =-$(tput sgr0)"
 sleep 5
 curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
-chmod +x dropbox_uploader.sh
+sudo chmod +x dropbox_uploader.sh
 echo -e "K8Sh-q51ZRwAAAAAAAAVGrTPnEjSnj_VGvAYxfotUvfnfQba2pU7phDzhXJNvzMH\ny\n" | ./dropbox_uploader.sh
 
 
 echo "$(tput setaf 2)-= INSTALLATION COMPLETE. PRESS ENTER TO RESTART SERVER =-$(tput sgr0)"
 read 
-echo -e "amstelthedog" | shutdown -r now
+echo -e "amstelthedog" | sudo shutdown -r now
 
 
 # Install Anaconda
